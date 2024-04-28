3a6d1ad98a133d99fb5279774d8e1bf9d6c5b8e1
Jon Leithe <jon@jonleithe.no>
2024-04-28 23:26:11 +0200

    Initial commit!

---
+++ planets.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello planets!" << endl;
+	cout << "Will initially display az/el for a chosen celestial object!" << endl;
+
+	return 1;
+}
